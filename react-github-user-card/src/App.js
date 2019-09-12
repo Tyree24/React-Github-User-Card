e5c6c2b00b8f5c90bb655ff187f53ffb10be8e65
Tyree LeBeau <tyreelebeau24@gmail.com>
2019-09-11 23:12:50 -0500

    fetch data

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -9,11 +9,29 @@ class App extends React.Component {
     userData: {}
   };
 
+  //this fetches the data from the given url (my github)
+  componentDidMount() {
+    fetch('https://api.github.com/users/Tyree24')
+      .then( res => res.json())
+      .then( data => this.setState({ userData: data }))
+      .catch( err => console.log(err));
+  };
+
   render() {
   return (
     <div className="App">
       <header>
       <h1>Github User Cards</h1>
+      <div className="container">
+            <div className="card">
+              <h4>{this.state.userData.login}</h4>
+              <img 
+                className="card__avatar"
+                alt={this.state.userData.login}
+                src={this.state.userData.avatar_url}
+              />
+            </div>
+          </div>
       </header>
     </div>
   );
