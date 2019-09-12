27d8c9e5d8985a6dd944348f3566ee5c43969391
Tyree LeBeau <tyreelebeau24@gmail.com>
2019-09-12 00:14:28 -0500

    fetch and renders followers

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    userData: {}
+    userData: {},
+    userFollowers: []
   };
 
   //this fetches the data from the given url (my github)
@@ -15,29 +16,56 @@ class App extends React.Component {
       .then( res => res.json())
       .then( data => this.setState({ userData: data }))
       .catch( err => console.log(err));
-  };
 
+      //this will fetch the data showing my followers (which is currently only one person)
+  fetch('https://api.github.com/users/Tyree24/followers')
+  .then( res => res.json())
+  .then( data => this.setState({ userFollowers: data }))
+  .catch( err => console.log(err));
+};
   render() {
   return (
     <div className="App">
       <header>
       <h1>Github User Cards</h1>
-      <div className="container">
-            <div className="card">
-              <h4>{this.state.userData.login}</h4>
 
-              <img 
-                className="card__avatar"
-                alt={this.state.userData.login}
-                src={this.state.userData.avatar_url}
-              />
-                            <p>Bio: {this.state.userData.bio}</p>
-              <p>Followers: {this.state.userData.followers}</p>
-            </div>
+      <div className="container">
+            <a href={this.state.userData.html_url} className="card user-card">
+              <div>
+                <img 
+                  className="card__avatar"
+                  alt={this.state.userData.login}
+                  src={this.state.userData.avatar_url}
+                />
+                <h4>{this.state.userData.login}</h4>
+                <p>Bio: {this.state.userData.bio}</p>
+                <p>Followers: {this.state.userData.followers}</p>
+              </div>
+            </a>
           </div>
-      </header>
-    </div>
-  );
-}
+
+          <h2>Followers</h2>
+
+{/* this will render out my followers (currently only one person) showing their username name and profile picture */}
+      <div className="container">
+      {this.state.userFollowers.map( follower => {
+              return (
+                <a href={follower.html_url} className="card">
+                  <div>
+                    <img 
+                      className="card__avatar"
+                      alt={follower.login}
+                      src={follower.avatar_url}
+                    />
+                    <h4>{follower.login}</h4>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </header>
+      </div>
+    );
+  };
 }
 export default App;
